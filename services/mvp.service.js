d3c6ac8f088f437a8c92ff9346ea4452cf7ddbde
Hamza Hussain <hamzaahussain67@gmail.com>
2023-05-06 23:44:29 +0500

    feat: added creds integration

--- services/mvp.service.js
+++ services/mvp.service.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 var resumeParser = require('../utils/parser');
 var { Configuration, OpenAIApi } = require('openai');
 require('dotenv').config();
@@ -14,6 +15,7 @@ function isGithub(key) {
 
 function _getGPTCompletion(prompt) {
   if (!prompt || !prompt.length) return;
+  console.log('prompt recieved', prompt);
 
   const configuration = new Configuration({
     apiKey: process.env.OPEN_AI_KEY,
@@ -27,6 +29,44 @@ function _getGPTCompletion(prompt) {
         content: `summarize the profile in 120 words by reading this text ahead without making assumptions, also rate the profile as beginner, intermediate, or advanced, keep it consice? \n${prompt}`,
       },
     ],
+  });
+}
+
+function _getCredsMetrics(rawResume) {
+  return new Promise(function (resolve, reject) {
+    const resume = rawResume
+      .replace(/[^a-z0-9\s]/gi, ' ')
+      .replace(/\t\n/g, ' ')
+      .replace(/\s+/g, ' ')
+      .toLowerCase()
+      .trim();
+    const requestData = { data: resume };
+    console.log(requestData);
+    const options = {
+      hostname: process.env.CREDS_ML_HOST,
+      port: process.env.CREDS_ML_PORT,
+      path: '/equivalence-text-lower-bound',
+      method: 'POST',
+      headers: {
+        'ngrok-skip-browser-warning': '69420',
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const request = http.request(options, function (response) {
+      let responseData = '';
+      response.on('data', (chunk) => {
+        responseData += chunk;
+      });
+      response.on('end', () => {
+        console.log(responseData);
+        resolve(JSON.parse(responseData));
+      });
+    });
+
+    request.on('error', (error) => reject(error));
+    request.write(JSON.stringify(requestData));
+    request.end();
   });
 }
 
@@ -51,11 +91,17 @@ function post(req, res, next) {
       if (err) throw err;
       var json = JSON.parse(data);
 
-      const GPTProfile = await _getGPTCompletion(resume.resume.rawText);
+      const [CREDS_METRICS, GPTProfile] = await Promise.all([
+        _getCredsMetrics(resume.resume.rawText),
+        _getGPTCompletion(resume.resume.rawText),
+      ]);
 
       const profile = {
         ...JSON.parse(data),
         profileSummary: GPTProfile.data.choices[0].message.content,
+        expertise: CREDS_METRICS.prediction,
+        expertiseLevel: CREDS_METRICS.complexity,
+        skills: CREDS_METRICS.skills,
       };
 
       res.render('result', {
